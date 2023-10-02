1182683056c23a031425bd6c973a99013cc32845
preslybenoit <okebenoithub@gmail.com>
2023-10-02 16:30:56 +0100

    State Jetpack Compose

---
+++ app/src/main/java/com/example/composestatedemo/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.composestatedemo
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composestatedemo.ui.theme.ComposeStateDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeStateDemoTheme {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    var count by rememberSaveable { mutableStateOf(0) }
+                    MyButton(count) { count = it + 1 }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyButton(currentCount: Int,updateCount:(Int) -> Unit) {
+    val context = LocalContext.current
+    Button(onClick = {
+        updateCount(currentCount)
+        Toast.makeText(context, "Count is : $currentCount", Toast.LENGTH_SHORT).show()
+    },
+        contentPadding = PaddingValues(16.dp),
+        border = BorderStroke(10.dp, Color.Black),
+        colors = ButtonDefaults.textButtonColors(
+            backgroundColor = Color.DarkGray,
+            contentColor = Color.White
+        )
+    ) {
+        Text("Count is : $currentCount",
+            style = MaterialTheme.typography.h3,
+            modifier = Modifier.padding(5.dp)
+        )
+    }
+}
+
